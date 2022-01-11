ff3470e56c2e57a18443d1d29a86da988ad34941
AidaiM <aidaim3004@gmail.com>
2022-01-11 22:45:19 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const myBurger = document.querySelector('.burger')
+const myMenu = document.querySelector('.menu')
+const menuLinks= document.querySelectorAll('.menu-link')
+
+myBurger.addEventListener('click', () => {
+     myMenu.classList.toggle('show')
+     document.body.classList.toggle('o-hidden')
+})
+
+menuLinks.forEach(onclick => {
+    onclick.addEventListener('click', () =>{
+        myMenu.classList.remove('show')
+        document.body.classList.remove('o-hidden')
+})
+})
